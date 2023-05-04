63bd9cf6254eccce8ac8b1663051d00ab4d0d2d0
bmhungqb <21522110@gm.uit.edu.vn>
2023-05-04 09:17:16 +0700

    add logic for login

--- FE_BookManagement/src/containers/Auth/components/loginSection.js
+++ FE_BookManagement/src/containers/Auth/components/loginSection.js
@@ -3,11 +3,62 @@ import { connect } from 'react-redux';
 import './loginSection.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { icon, faa } from '@fortawesome/fontawesome-svg-core/import.macro'
-import { } from '@fortawesome/free-solid-svg-icons';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { event } from 'jquery';
+import e from 'cors';
 class LoginSection extends Component {
     constructor(props) {
         super(props)
         this.state = {
+            username: "",
+            password: "",
+            isShowPassword: false,
+            errMessage: ""
+        }
+    }
+    handleOnchangeUsername = (event) => {
+        this.setState({
+            username: event.target.value
+        })
+    }
+    handleOnchangePassword = (event) => {
+        this.setState({
+            password: event.target.value
+        })
+    }
+    handleKeyDown = (event) => {
+        if (event.key === "Enter" || event.keyCode === 13) {
+            this.handleLogin();
+        }
+    }
+    handleShowHidePassword = () => {
+        this.setState({
+            isShowPassword: !this.state.isShowPassword
+        })
+    }
+    handleLogin = async () => {
+        this.setState({
+            errMessage: ''
+        })
+        try {
+            let data = handleLoginAPI(this.state.username, this.state.password);
+            if (data && data.errCode !== 0) {
+                this.setState({
+                    errMessage: data.message
+                })
+            }
+            if (data && data.errCode === 0) {
+                this.props.userLoginSuccess(data.user)
+            }
+        }
+        catch (error) {
+            if (error.response) {
+                if (error.response.data) {
+                    this.setState({
+                        errMessage: error.response.data.message
+                    })
+                }
+            }
         }
     }
     render() {
@@ -20,37 +71,72 @@ class LoginSection extends Component {
                     </div>
                     <div className='login-content'>
                         {/* Login with google */}
-                        <div className='login-with-google'>
+                        {/* <div className='login-with-google'>
                             <button type='button' className='btn btn-lg btn-outline-primary w-100 mb-1'>
                                 <p>Sign in with Google</p>
-                                {/* <FontAwesomeIcon icon={faGoogleLogo} /> */}
                             </button>
-                        </div>
+                        </div> */}
                         <div className='login-user-pass'>
                             <form>
                                 <div className="form-group mt-3">
-                                    <label className='font-weight-bold'>Email</label>
-                                    <input type="email" className="form-control form-control-lg" placeholder="Enter email" />
+                                    <label className='font-weight-bold'>Username</label>
+                                    <input
+                                        type="email"
+                                        className="form-input form-control form-control-lg"
+                                        placeholder="Enter your username"
+                                        value={this.state.username}
+                                        onChange={(e) => { this.handleOnchangeUsername(e) }}
+                                    />
                                 </div>
                                 <div className="form-group">
                                     <label className='font-weight-bold'>Password</label>
-                                    <input type="password" className="form-control form-control-lg" placeholder="Password" />
+                                    <div className='input-group d-flex'>
+                                        <input
+                                            type="password"
+                                            className="form-input form-control form-control-lg py-2 border-right-0 border"
+                                            placeholder="Enter your password"
+                                            value={this.state.password}
+                                            onChange={(e) => { this.handleOnchangePassword(e) }}
+                                            onKeyDown={(e) => { this.handleKeyDown(e) }}
+                                        />
+                                        <span className="input-group-append">
+                                            <button
+                                                className="btn btn-outline-secondary border-left-0 border"
+                                                type="button"
+                                                onClick={() => { this.handleShowHidePassword() }}
+                                            >
+                                                <FontAwesomeIcon
+                                                    icon={this.state.isShowPassword ? faEyeSlash : faEye}
+                                                />
+                                            </button>
+                                        </span>
+                                    </div>
+
                                 </div>
                                 <div className='remember-forget d-flex justify-content-between mb-2 align-items-center'>
-                                    <div className="form-check pl-0 d-flex">
-                                        <input type="checkbox" />
+                                    <div className="form-check d-flex">
+                                        <input type="checkbox" className='form-check-input' />
                                         <label className="form-check-label ml-2">Remember me</label>
                                     </div>
                                     <p className='font-weight-bold'>Forgot password?</p>
                                 </div>
-                                <button type="submit" className="btn-submit btn btn-lg btn-outline-primary w-100">Submit</button>
+                                <div className='show-error'>
+                                    {this.state.errMessage}
+                                </div>
+                                <button
+                                    type="submit"
+                                    className="btn-submit btn btn-lg btn-outline-primary w-100 mt-3"
+                                    onClick={() => { this.handleLogin() }}
+                                >
+                                    Login
+                                </button>
                             </form>
                         </div>
                     </div>
-                    <div className='login-footer d-flex mt-4'>
+                    {/* <div className='login-footer d-flex mt-4'>
                         <p className='font-weight-bold'>Not regestered yet?</p>
                         <a className='ml-2'>Create a new account</a>
-                    </div>
+                    </div> */}
                 </div>
             </Fragment>
         );
