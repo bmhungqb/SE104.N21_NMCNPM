a5df8b3595e0aa6e8baa70eada034d8af20ad274
bmhungqb <21522110@gm.uit.edu.vn>
2023-05-06 09:00:26 +0700

    fix sidebar active

--- FE_BookManagement/src/containers/SideBar/sideBar.js
+++ FE_BookManagement/src/containers/SideBar/sideBar.js
@@ -1,32 +1,39 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom/cjs/react-router-dom.min';
 import { connect } from 'react-redux';
 import { FormattedMessage } from 'react-intl';
 import "./sideBar.scss"
+import { Link } from 'react-router-dom/cjs/react-router-dom';
+import { NavLink } from 'react-router-dom';
 class SideBar extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isActive: false
+        }
+    }
     render() {
         return (
             <React.Fragment>
                 <div className="border-end bg-white" id="sidebar-wrapper">
                     <div className="sidebar-heading border-bottom bg-light">
-
                     </div>
                     <div className="list-group list-group-flush">
-                        <a className="list-group-item list-group-item-action list-group-item-light p-3" href="/home">Home</a>
-                        <a className="list-group-item list-group-item-action list-group-item-light p-3" href="/book-management">Book Management</a>
-                        <a className="list-group-item list-group-item-action list-group-item-light p-3" href="/book-purchase">Book Purchase</a>
-                        <a className="list-group-item list-group-item-action list-group-item-light p-3" href="/book-rental">Book Rental</a>
-                        <a className="list-group-item list-group-item-action list-group-item-light p-3" href="/customer-management">Customer Management</a>
-                        <a className="list-group-item list-group-item-action list-group-item-light p-3" href="/supplier-management">Supplier Management</a>
-                        <a className="list-group-item list-group-item-action list-group-item-light p-3" href="/user-management">Users</a>
-                        <a className="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Discount code</a>
-                        <a className="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Conversation</a>
-                        <a className="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Notifications</a>
-                        {/* line here */}
-                        <a className="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Get Technical Help</a>
-                        <a className="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Settings</a>
+                        <NavLink className="list-group-item list-group-item-action list-group-item-light p-3" to='/home'>Home</NavLink>
+                        <NavLink className="list-group-item list-group-item-action list-group-item-light p-3" to='/book-management'>Book Management</NavLink>
+                        <NavLink className="list-group-item list-group-item-action list-group-item-light p-3" to="/book-purchase">Book Purchase</NavLink>
+                        <NavLink className="list-group-item list-group-item-action list-group-item-light p-3" to="/book-rental">Book Rental</NavLink>
+                        <NavLink className="list-group-item list-group-item-action list-group-item-light p-3" to="/customer-management">Customer Management</NavLink>
+                        <NavLink className="list-group-item list-group-item-action list-group-item-light p-3" to="/supplier-management">Supplier Management</NavLink>
+                        <NavLink className="list-group-item list-group-item-action list-group-item-light p-3" to="/user-management">Users</NavLink>
+                        <NavLink className="list-group-item list-group-item-action list-group-item-light p-3" to="/customer-">Discount code</NavLink>
+                        <NavLink className="list-group-item list-group-item-action list-group-item-light p-3" to="/conversation">Conversation</NavLink>
+                        <NavLink className="list-group-item list-group-item-action list-group-item-light p-3" to="/notifications">Notifications</NavLink>
+                        <NavLink className="list-group-item list-group-item-action list-group-item-light p-3" to="/technical-help">Get Technical Help</NavLink>
+                        <NavLink className="list-group-item list-group-item-action list-group-item-light p-3" to="/s">Settings</NavLink>
                     </div>
                 </div>
-            </React.Fragment>
+            </React.Fragment >
         );
     }
 
